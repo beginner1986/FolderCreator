922547e3a944c1a313e259b4ff46fd7f47e3d83f
beginner1986 <beginner1986@gmail.com>
2025-08-22 21:29:36 +0200

    Template details displayed in ListView

--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -1,15 +1,5 @@
-﻿using FolderCreator.Models;
-using FolderCreator.ViewModels;
-using System.Text;
+﻿using FolderCreator.ViewModels;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace FolderCreator
 {
@@ -18,24 +8,6 @@ namespace FolderCreator
         public MainWindow()
         {
             InitializeComponent();
-
-            Template template = new Template
-            {
-                Name = "DefaultTemplate",
-                Folders = new List<TemplateFolder>
-                {
-                    new TemplateFolder
-                    {
-                        Path = "Folder1"
-                    },
-                    new TemplateFolder
-                    {
-                        Path = "Folder2"
-                    }
-                },
-            };
-
-            TemplateManager.SaveTemplate(template);
 
             MainViewModel mainViewModel = new MainViewModel();
             this.DataContext = mainViewModel;
